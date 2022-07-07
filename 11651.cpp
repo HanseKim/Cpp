1173fe843cc9c592bb73504d2ffda9360c743e36
HanseKim <kimbird2002@naver.com>
2022-07-07 17:09:46 +0900

    11651

---
+++ 11651.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+vector< pair<int,int> > v;
+
+bool compare(pair<int,int> a, pair<int,int>b){
+    if(a.second==b.second) return a.first < b.first;
+    else return a.second<b.second;
+}
+
+int main(){
+    int n;scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int x,y; scanf("%d %d",&x,&y);
+        v.push_back(make_pair(x,y));
+    }
+    sort(v.begin(),v.end(),compare);
+    for(int i=0;i<n;i++){
+        printf("%d %d\n",v[i].first,v[i].second);
+    }
+}
