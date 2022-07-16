4e2d24b18f161dba8224df67927c4e0c8f665b95
HanseKim <kimbird2002@naver.com>
2022-07-17 00:22:23 +0900

    7662

--- 7662 이중 우선순위 큐.cpp
+++ 7662 이중 우선순위 큐.cpp
@@ -1,13 +1,23 @@
+/**
+ * @file 7662 이중 우선순위 큐.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-17
+ * @see https://www.acmicpc.net/problem/7662
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include<iostream>
 #include<queue>
 #include<vector>
-#include<set>
+#include<set>   //multiset
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    multiset<int> ms;
+    multiset<int> ms;  //중복이 허용되는 걸 사용하기 위해 multiset을 사용
     int t; cin>>t;
     while(t--){
         ms.clear();
@@ -26,6 +36,7 @@ int main(){
                 }
                 else if(n == 1){
                     ms.erase(--ms.end());
+                    //m.end()가 마지막 원소가 끝나는 주소값을 반환하기 때문에 --를 붙인다.
                 }
                 else if(n==-1){
                     ms.erase(ms.begin());
