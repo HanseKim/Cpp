5448cc1a8eb21938585fd01663fd0d9c261b2ffc
HanseKim <kimbird2002@naver.com>
2022-07-16 15:39:53 +0900

    18870

--- 18870 좌표압축.cpp
+++ 18870 좌표압축.cpp
@@ -1,22 +1,50 @@
-#include	<iostream>
-#include	<map>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
+vector<pair<int,int>> v;
+vector<pair<int,int>> v2;
+
+bool cmp_n(pair<int,int> v1,pair<int,int> v2){
+	if(v1.first < v2.first) return v1.first < v2.first;
+	else return v1.first > v2.first;
+}
+bool cmp_idx(pair<int,int> v1,pair<int,int> v2){
+	if(v1.second < v2.second) return v1.second < v2.second;
+	else return v1.second > v2.second;
+}
 int main(){
-	map <int, int> m;
-	map <int, int> :: iterator iter;
-	int n; cin>>n;
-	int a[n];
+	int n; scanf("%d",&n);
 	for(int i=0;i<n;i++){
-		cin>>a[i];
-		m.insert({a[i],i});
+		int k; scanf("%d",&k);
+		v.push_back({k,i});
 	}
-	int index=0;
-	for (iter = m.begin(); iter != m.end(); ++iter) {
-        if(iter == m.begin()) iter->second = index++;
-        else iter->second = index++;
-	}
+	sort(v.begin(),v.end());
+	/*
 	for(int i=0;i<n;i++){
-		cout<<m[a[i]]<<" ";
+		printf("<f:%d s:%d> ",v[i].first,v[i].second);
 	}
+	cout<<"\n";
+	*/
+	int cnt=0;
+	for(int i=0;i<n;i++){
+		if(i==0){
+			v2.push_back({v[i].second,cnt});
+		}
+		else if(v[i-1].first == v[i].first){
+			v2.push_back({v[i].second,cnt});
+		}
+		else if(v[i-1].first < v[i].first){
+			v2.push_back({v[i].second,cnt+1});
+			cnt++;
+		}
+	}
+	
+
+	sort(v2.begin(),v2.end());
+	for(int i=0;i<n;i++){
+		printf("%d ",v2[i].second);
+	}
+	cout<<"\n";
 }
